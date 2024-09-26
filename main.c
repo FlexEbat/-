46cdc1159daeaae09b6cbb5b85320a86f47238b5
FlexEbat <52438328+FlexEbat@users.noreply.github.com>
2024-09-26 16:50:27 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
-#include "stdio.h"
+#include <stdio.h>        // подключаем заголовочный файл stdio.h
+ 
+/*
+TEXT
+*/
 
-
-
-int main () {
-	printf("hello");
-}
+int main(void)                  // определяем функцию main
+{                               // начало функции
+    printf("Hello METANIT.COM!");       // выводим строку на консоль
+    return 0;                   // выходим из функции
+}                               // конец функции
